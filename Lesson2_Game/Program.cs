579d03f2575d92e2e4f30ec43e72348eb703ec39
Prusaq <Prusaq@yandex.ru>
2022-07-15 19:18:41 +0400

    v.2

---
+++ Lesson2_Game/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson2_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
