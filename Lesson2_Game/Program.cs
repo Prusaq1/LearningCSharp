2d3408d9f76ec041148824988d588b9a84d9d583
Prusak1 <Prusaq@yandex.ru>
2022-07-22 17:20:00 +0400

    2207

--- Lesson2_Game/Program.cs
+++ Lesson2_Game/Program.cs
@@ -1,17 +1,13 @@
 ﻿namespace ConsoleGame
 {
-           internal class ProgramBase
+    internal class ProgramBase
     {
 
-        static readonly char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-        static int player = 1; 
-        static int choice; //Если флаг = 1 то ктото выиграл
-                           //Если флаг = -1 то ничья 
-                           //Если флаг = 0 то играем
-        static int flag = 0;
-
+        static readonly char[] XO = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static int player = 1;
+        static int choice;
+        static int flag = 0; // Усли = 1 то ктото выиграл/ = -1 то Ничья/ 0 то играем
         public ProgramBase() { }
-
         static void Main(string[] args)
         {
             do
@@ -19,7 +15,7 @@
                 Console.Clear();// Очистка экрана
                 Console.WriteLine("Игрок 1:X and Игрок 2:O");
                 Console.WriteLine("\n");
-                if (player % 2 == 0)//checking the chance of the player
+                if (player % 2 == 0)//Проверка очерёдности
                 {
                     Console.WriteLine("Игрок 2 твой ход");
                 }
@@ -31,42 +27,36 @@
                 Board();
                 choice = int.Parse(Console.ReadLine());//Выбор пользователя
                                                        // проверка возможности хода (свободное поле)
-                if (arr[choice] != 'X' && arr[choice] != 'O')
+                if (XO[choice] != 'X' && XO[choice] != 'O')
                 {
                     if (player % 2 == 0) //Если ход игрока 2 то возвращается 0, иначе возвращается Х
                     {
-                        arr[choice] = 'O';
+                        XO[choice] = 'O';
                         player++;
                     }
                     else
                     {
-                        arr[choice] = 'X';
+                        XO[choice] = 'X';
                         player++;
                     }
                 }
                 else
-                //If there is any possition where user want to run
-                //and that is already marked then show message and load board again
+                //Если занято, показать сообщение, изаново ждать ход
                 {
-                    Console.WriteLine("Sorry the row {0} is already marked with {1}", choice, arr[choice]);
-                    Console.WriteLine("\n");
-                    Console.WriteLine("Please wait 2 second board is loading again.....");
-                    Thread.Sleep(2000);
+                    Console.WriteLine("------ЗАНЯТО!------\nВыберите другое поле\n-----через 3 сек.-----");
+                    Thread.Sleep(3000);
                 }
-                flag = CheckWin();// calling of check win
+                flag = CheckWin();
             }
-            while (flag != 1 && flag != -1);
-            // This loop will be run until all cell of the grid is not marked
-            //with X and O or some player is not win
-            Console.Clear();// clearing the console
-            Board();// getting filled board again
+            while (flag != 1 && flag != -1);            // Цикл заполнения всех полей
+            Console.Clear();
+            Board();// возврат заполненных полей
             if (flag == 1)
-            // if flag value is 1 then someone has win or
-            //means who played marked last time which has win
+            // Если =1 последний ход выиграл
             {
                 Console.WriteLine("Игрок {0} Выграл!", (player % 2) + 1);
             }
-            else// если flag -1 то ничья
+            else
             {
                 Console.WriteLine("   Ничья");
             }
@@ -75,69 +65,56 @@
         private static void Board()
         {
             Console.WriteLine("     |     |      ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
+            Console.WriteLine("  {0}  |  {1}  |  {2}", XO[1], XO[2], XO[3]);
             Console.WriteLine("_____|_____|_____ ");
             Console.WriteLine("     |     |      ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
+            Console.WriteLine("  {0}  |  {1}  |  {2}", XO[4], XO[5], XO[6]);
             Console.WriteLine("_____|_____|_____ ");
             Console.WriteLine("     |     |      ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
+            Console.WriteLine("  {0}  |  {1}  |  {2}", XO[7], XO[8], XO[9]);
             Console.WriteLine("     |     |      ");
         }
-        //Checking that any player has won or not
+        //проверка на выиграш
         private static int CheckWin()
         {
-            #region Horzontal Winning Condtion
-            //Winning Condition For First Row
-            if (arr[1] == arr[2] && arr[2] == arr[3])
+            //Условия выиграша рядов
+            if (XO[1] == XO[2] && XO[2] == XO[3])
             {
                 return 1;
             }
-            //Winning Condition For Second Row
-            else if (arr[4] == arr[5] && arr[5] == arr[6])
+            else if (XO[4] == XO[5] && XO[5] == XO[6])
             {
                 return 1;
             }
-            //Winning Condition For Third Row
-            else if (arr[6] == arr[7] && arr[7] == arr[8])
+            else if (XO[6] == XO[7] && XO[7] == XO[8])
             {
                 return 1;
             }
-            #endregion
-            #region vertical Winning Condtion
-            //Winning Condition For First Column
-            else if (arr[1] == arr[4] && arr[4] == arr[7])
+            else if (XO[1] == XO[4] && XO[4] == XO[7])
             {
                 return 1;
             }
-            //Winning Condition For Second Column
-            else if (arr[2] == arr[5] && arr[5] == arr[8])
+            else if (XO[2] == XO[5] && XO[5] == XO[8])
             {
                 return 1;
             }
-            //Winning Condition For Third Column
-            else if (arr[3] == arr[6] && arr[6] == arr[9])
+            else if (XO[3] == XO[6] && XO[6] == XO[9])
             {
                 return 1;
             }
-            #endregion
-            #region Diagonal Winning Condition
-            else if (arr[1] == arr[5] && arr[5] == arr[9])
+            else if (XO[1] == XO[5] && XO[5] == XO[9])
             {
                 return 1;
             }
-            else if (arr[3] == arr[5] && arr[5] == arr[7])
+            else if (XO[3] == XO[5] && XO[5] == XO[7])
             {
                 return 1;
             }
-            #endregion
-            #region Checking For Draw
-            // If all the cells or values filled with X or O then any player has won the match
-            else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' && arr[6] != '6' && arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
+
+            else if (XO[1] != '1' && XO[2] != '2' && XO[3] != '3' && XO[4] != '4' && XO[5] != '5' && XO[6] != '6' && XO[7] != '7' && XO[8] != '8' && XO[9] != '9')
             {
-                return -1;
+                return -1; // Если все клетки заполнены то ничья
             }
-            #endregion
             else
             {
                 return 0;
