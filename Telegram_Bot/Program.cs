f7a4fc7cb86bb839034213043be9de54d8f0b606
Prusaq1 <109171674+Prusaq1@users.noreply.github.com>
2022-10-11 17:08:40 +0400

    1110

--- Telegram_Bot/Program.cs
+++ Telegram_Bot/Program.cs
@@ -2,74 +2,126 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
-using Telegram.Bot.Types;
 using Telegram.Bot.Exceptions;
-using Telegram.Bot.Polling;
-using NLog;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Framework.Abstractions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 
-namespace TelegramBotExperiments
+var botClient = new TelegramBotClient("5526082175:AAGC9NaazJ5BGrgQ6u2kApQnnN8aSU71Vwo");
+
+using var cts = new CancellationTokenSource();
+
+var receiverOptions = new ReceiverOptions
 {
-    class Program
+    AllowedUpdates = { }
+};
+
+botClient.StartReceiving(
+    HandleUpdatesAsync,
+    HandleErrorAsync,
+    receiverOptions,
+    cancellationToken: cts.Token);
+User me = await botClient.GetMeAsync();
+
+Console.WriteLine("Запущен бот " + botClient.GetMeAsync().Result.FirstName);
+Console.ReadLine();
+
+cts.Cancel();
+
+async Task HandleUpdatesAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+
+    if (update.Type == UpdateType.Message && update?.Message?.Text != null)
     {
-        private static ITelegramBotClient bot = new TelegramBotClient("5526082175:AAGC9NaazJ5BGrgQ6u2kApQnnN8aSU71Vwo");
-        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
-        {
-            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
-            {
-                var message = update.Message;
-                Console.WriteLine($"{message.Chat.FirstName} | {message.Text}");
-                if (message.Text.ToLower() == "привет")
-                {
-                    await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать!");
-                    return;
-                }
-                await botClient.SendTextMessageAsync(message.Chat.Id, "Привет!");
-            }
-        }
-         //static void SendInline(long chatId, CancellationToken cancellationToken)
-         //   InlineKeyboardMarkup inlineKeyboard = new(new[])
-         //   { new[]
-         //   {
-         //       InlineKeyboardButton.WithCallbackData(text: "Кнопка_1", callbackData: "key_1"),
-         //       InlineKeyboardButton.WithCallbackData(text: "Кнопка_2", callbackData: "key_2"),
-         //   },
-         //   };
-
-
-
-        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
-        {
-             Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
-        }
-        
-        //private async Task HandlUpdate
-
-        //{
-        //if (Update != null)
-        //    {
-        //    var masage = update.mesage;
-
-        
-
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
-
-            var cts = new CancellationTokenSource();
-            var cancellationToken = cts.Token;
-            var receiverOptions = new ReceiverOptions
-            {
-                AllowedUpdates = { }, // receive all update types
-            };
-            bot.StartReceiving(
-                HandleUpdateAsync,
-                HandleErrorAsync,
-                receiverOptions,
-                cancellationToken
-            );
-            Console.ReadLine();
-        }
+        await HandleMessage(botClient, update.Message);
+        return;
     }
+
+    if (update.Type == UpdateType.CallbackQuery)
+    {
+        await HandleCallbackQuery(botClient, update.CallbackQuery);
+        return;
+    }
+}
+
+async Task HandleMessage(ITelegramBotClient botClient, Message message)
+{
+    if (message.Text == "/start")
+    {
+        await botClient.SendTextMessageAsync(message.Chat.Id, "Choose commands: /inline | /keyboard");
+        return;
+    }
+
+    if (message.Text == "/keyboard")
+    {
+        ReplyKeyboardMarkup keyboard = new(new[]
+        {
+            new KeyboardButton[] {"Button_1", "Button_2"},
+            new KeyboardButton[] {"Кнопка_1", "Копка_2" }
+        })
+        {
+            ResizeKeyboard = true
+        };
+        await botClient.SendTextMessageAsync(message.Chat.Id, "Choose:", replyMarkup: keyboard);
+        return;
+    }
+
+    if (message.Text == "/inline")
+    {
+        InlineKeyboardMarkup keyboard = new(new[]
+        {
+            new[]
+            {
+                InlineKeyboardButton.WithCallbackData("Data_1", "data_1"),
+                InlineKeyboardButton.WithCallbackData("Data_2", "data_2"),
+            },
+            new[]
+            {
+                InlineKeyboardButton.WithCallbackData("Data_3", "data_3"),
+                InlineKeyboardButton.WithCallbackData("Data_4", "data_4"),
+            },
+        });
+        await botClient.SendTextMessageAsync(message.Chat.Id, "Choose inline:", replyMarkup: keyboard);
+        return;
+    }
+
+    await botClient.SendTextMessageAsync(message.Chat.Id, $"You said:\n{message.Text}");
+}
+async Task HandleCallbackQuery(ITelegramBotClient botClient, CallbackQuery callbackQuery)
+{
+    if (callbackQuery.Data.StartsWith("buy"))
+    {
+        await botClient.SendTextMessageAsync(
+            callbackQuery.Message.Chat.Id,
+            $"Вы хотите купить?"
+        );
+        return;
+    }
+    if (callbackQuery.Data.StartsWith("sell"))
+    {
+        await botClient.SendTextMessageAsync(
+            callbackQuery.Message.Chat.Id,
+            $"Вы хотите продать?"
+        );
+        return;
+    }
+    await botClient.SendTextMessageAsync(
+        callbackQuery.Message.Chat.Id,
+        $"You choose with data: {callbackQuery.Data}"
+        );
+    return;
+}
+
+Task HandleErrorAsync(ITelegramBotClient botclient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Ошибка телеграм АПИ:\n{apiRequestException.ErrorCode}\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
 }
